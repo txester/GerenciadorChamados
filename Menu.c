d07f44cd31610a9ae4c2b3074c288f734f4543c0
Ester Teixeira <ester.augusta.teixeira@gmail.com>
2019-11-03 16:12:21 -0300

    Ajuste De Menu

--- Menu.c
+++ Menu.c
@@ -6,27 +6,24 @@ int menu = -1;
 void menuCliente(){ //EVITAR REPETIÇÃO DE CONDIGO.
     printf("Menu>1 Cliente\n");
     printf("---------------------\n");
-    printf("1-Criar chamados\n");
-    printf("2-Acompanhar chamados\n");
-    printf("3-Remover Chamados\n");
-    printf("4-Voltar\n");
-    printf("0-Sair\n");
+    printf("1 - Criar chamados\n");
+    printf("2 - Acompanhar chamados\n");
+    printf("3 - Remover Chamados\n");
+    printf("4 - Voltar\n");
+    printf("0 - Sair\n");
 
-    int menucliente = -1;
-    printf("Selecione Um Menu\n");
-    scanf("%i",&menucliente);
+    int menuCliente = -1;
+    printf("Selecione um menu: ");
+    scanf("%i",&menuCliente);
+    system ("cls");
 
-    system ("cls")
-
-
-    switch(menucliente){
-
-
+    switch(menuCliente){
     case 1:
-        criarChamados();
+        printf("1 - Criar chamados");
         break;
+
     case 2:
-        acompanharChamados();
+        printf("2 - Acompanhar chamados");
         break;
 
     case 3:
@@ -39,37 +36,53 @@ void menuCliente(){ //EVITAR REPETIÇÃO DE CONDIGO.
 
     case 0:
         menu = 0;
-        break
+        break;
 
-            default:
     }
-
-
-
 
 };
 
 void menuGerente(){
     printf("Menu>2 Gerente\n");
     printf("---------------------\n");
-    printf("1- Acompanhar chamados\n");
-    printf("2-Alterar chamados\n");
-    printf("3-Agendar Visita Tecnica\n");
-    printf("Relatorios\n");
-    printf("4-Voltar\n");
+    printf("1 - Acompanhar chamados\n");
+    printf("2 - Alterar chamados\n");
+    printf("3 - Agendar Visita Tecnica\n");
+    printf("4 - Relatorios\n");
+    printf("5 - Voltar\n");
     printf("0-Sair\n");
 
-    int menugerente = -1;
-    printf("Selecione Um Menu\n");
-    scanf("%i",&menugerente);
+    int menuGerente = -1;
+    printf("Selecione Um Menu: ");
+    scanf("%i",&menuGerente);
+    system ("cls");
 
-    system ("cls")
+    switch(menuGerente){
+    case 1:
+        printf("1 - Acompanhar chamados");
+        break;
 
+    case 2:
+        printf("2 - Alterar chamados");
+        break;
 
+    case 3:
+        printf("3 - Agendar Visita Tecnica");
+        break;
 
+    case 4:
+        printf("4 - Relatorios");
+        break;
 
+    case 5:
+        menu = -1;
+        break;
 
-};
+    case 0:
+        menu = 0;
+        break;
+    }
+}
 
 void menuPrincipal(){
     printf("Menu\n");
